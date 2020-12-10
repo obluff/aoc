d083e33752eb2fb646fd54d7bcd34e42d394ef00
Ian Wright <ianwrightaw@gmail.com>
2020-12-10 10:59:33 -0500

    hooohoohohohohohohoho

--- 10/p1.sh
+++ 10/p1.sh
@@ -1,4 +1,3 @@
-expr $(expr 1 + $(paste <(sort -n input.txt | head -n -1) <(sort -n input.txt | tail -n +2) | awk '{print $2-$1}' | grep "1" | wc -l))  \
-\* $(expr 1 + $(paste <(sort -n input.txt | head -n -1) <(sort -n input.txt | tail -n +2) | awk '{print $2-$1}' | grep "3" | wc -l ))
+paste <(sort -n input.txt | head -n -1) <(sort -n input.txt | tail -n +2) | awk '{print $2-$1}' | sort | uniq -c | awk 'BEGIN {product = 1}{ product*= $1 + 1} END {print product}'
 
 
